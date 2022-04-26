c1f784d8c1d478abe0ded53cb5634cac79f1183c
Alex V <alexwolf2017@outlook.com>
2022-04-26 21:17:49 +0300

    fixed cargo test

--- adder/tests/adder_mandos_rs_test.rs
+++ adder/tests/adder_mandos_rs_test.rs
@@ -2,7 +2,6 @@ use elrond_wasm_debug::*;
 
 fn world() -> BlockchainMock {
     let mut blockchain = BlockchainMock::new();
-    blockchain.set_current_dir_from_workspace("contracts/examples/adder");
 
     blockchain.register_contract_builder("file:output/adder.wasm", adder::ContractBuilder);
     blockchain
